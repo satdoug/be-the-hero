6a9edbcf467dcc8e5427fddfc268e1ab219228b8
Douglas.Santos <douglas.santos@softplan.com.br>
2020-03-28 22:49:50 -0300

    Adicionando Testes Unitários e Integração

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const { errors } = require('celebrate');
 
 const app = express();
 
@@ -22,7 +23,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 
-app.listen(3333);
+module.exports = app;
 
